7f1f431ffa3a137be82eeb81a14142150d6c8a91
Lucas Cairns <lucas.cairns@gmail.com>
2021-06-18 11:57:15 +0100

    Add reordering of groups (#20)
    - Add endpoint to accept requested group order
    - Update clientside code to post request
    - Update templates to include button and error handling

--- assets/js/questionGroup.js
+++ assets/js/questionGroup.js
@@ -1,14 +1,69 @@
 ;(function () {
   const table = document.getElementById('questionGroups')
 
+  let order = []
+  let listItems = document.getElementsByClassName('question-group')
+
+  const saveButtonDefaultText = 'Save group order'
+  const saveButton = document.getElementById('save-group-order')
+  const saveButtonError = document.getElementById('save-group-order-error')
+
+  saveButton.setAttribute('disabled', 'disabled')
+  saveButton.innerText = saveButtonDefaultText
+
+  for (const item of listItems) {
+    order.push(item.dataset.id)
+  }
+
   Sortable.create(table, {
     draggable: '.draggable',
     ghostClass: 'draggable--ghost', // Class name for the drop placeholder
     chosenClass: 'draggable--chosen', // Class name for the chosen item
     dragClass: 'draggable--move', // Class name for the dragging item
-    onUpdate: function () {
-      const order = this.toArray()
-      console.log(order)
+    onChange: function () {
+      order = this.toArray()
+      saveButton.removeAttribute('disabled')
+      saveButton.innerText = saveButtonDefaultText
+      console.log('changed')
     },
   })
+
+  if (saveButton) {
+    const saveGroupOrder = function (event) {
+      event.preventDefault()
+
+      console.log(order)
+
+      const token = document.getElementById('csrf').dataset.csrf
+      const group = document.getElementById('group-uuid').dataset.group
+
+      const req = new XMLHttpRequest()
+
+      req.addEventListener('load', function (res) {
+        if (this.status !== 200) {
+          console.log('None 200 response when updating group order')
+          saveButtonError.classList.remove('govuk-visually-hidden')
+          saveButtonError.innerText = this.responseText || 'Something went wrong updating the group'
+          return
+        }
+        saveButtonError.classList.add('govuk-visually-hidden')
+        saveButton.setAttribute('disabled', 'disabled')
+        saveButton.innerText = 'Group updated'
+      })
+
+      req.addEventListener('error', function () {
+        console.log('Request to update failed')
+        saveButtonError.classList.remove('govuk-visually-hidden')
+        saveButtonError.innerText = 'Request failed'
+      })
+
+      req.open('POST', '/questions/group/' + group + '/update')
+      req.setRequestHeader('Content-Type', 'application/json;charset=UTF-8')
+      req.setRequestHeader('X-CSRF-Token', token)
+      req.withCredentials = true
+      req.send(JSON.stringify(order))
+    }
+
+    saveButton.onclick = saveGroupOrder
+  }
 })()
